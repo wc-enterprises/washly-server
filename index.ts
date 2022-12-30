1c3fea993725f75a1753c26c615ef9a1651e9255
abin <abin@surfboard.se>
2022-12-30 11:40:18 +0530

    project configs

--- index.ts
+++ index.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import { AppModule } from './src/app.module';
+
+admin.initializeApp({
+  credential: admin.credential.cert(functions.config().washly_config),
+  databaseURL: 'https://laundry-guru-50b7a.firebaseio.com',
+});
 const expressServer = express();
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
